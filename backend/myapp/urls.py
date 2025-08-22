3c871660c2b01fdb7c41b0c3da0c45a9a7bd283e
David-Yan-22 <73031516+David-Yan-22@users.noreply.github.com>
2025-08-22 13:06:30 +1000

    some trials on backend

--- backend/myapp/urls.py
+++ backend/myapp/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path("", views.index, name="index"),
+    path("assignments", views.show_assignments, name="assignments"),
 ]
--- backend/myapp/views.py
+++ backend/myapp/views.py
@@ -4,3 +4,14 @@ from django.http import HttpResponse, JsonResponse
 # Create your views here.
 def index(request):
     return JsonResponse({ "message" : "hello, world!"})
+
+
+def show_assignments(request):
+    return JsonResponse({ "message" : "hello, world!"})
+
+def delete_assignment(request):
+    return JsonResponse({"message" : "Assignment is deleted"})
+
+def create_assignment(request):
+    print("Assignment is created")
+    return JsonResponse({"message" : "Assignment is created"})
