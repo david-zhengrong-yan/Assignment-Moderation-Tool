6f8df6b818bda62d11057fd6bb10192e23e2ca87
David-Yan-22 <73031516+David-Yan-22@users.noreply.github.com>
2025-08-30 17:52:42 +1000

    database init

--- backend/myapp/models.py
+++ backend/myapp/models.py
@@ -1,3 +1,20 @@
 from django.db import models
 
+
+class Administrator():
+    pass
+
+class Marker():
+    pass
+
+
+class Assignment(models.Model):
+    pass
+
+class Submission():
+    pass
+
 # Create your models here.
+class Subject(models.Model):
+    name = models.CharField()
+    code = models.CharField()
