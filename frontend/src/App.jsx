05cbd8b481bc7be6bf420fc635efdef52144c2e2
Simon Song <simonsong0816@gmail.com>
2025-09-12 13:48:43 +1000

    create assignment page

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,27 +1,29 @@
 // import { useState } from 'react'
 // import reactLogo from './assets/react.svg'
 // import viteLogo from '/vite.svg'
-import './App.css';
+import "./App.css";
 import { Route, Routes, Navigate } from "react-router";
-import SignupPage from './pages/SignupPage';
-import HomePage from './pages/HomePage';
-import LoginPage from './pages/LoginPage';
-import PeoplePage from './pages/PeoplePage';
-import AccountPage from './pages/AccountPage';
-import EditAccountPage from './pages/EditAccountPage';
-import AssignmentPage from './pages/AssignmentPage';
+import SignupPage from "./pages/SignupPage";
+import HomePage from "./pages/HomePage";
+import LoginPage from "./pages/LoginPage";
+import PeoplePage from "./pages/PeoplePage";
+import AccountPage from "./pages/AccountPage";
+import EditAccountPage from "./pages/EditAccountPage";
+import AssignmentPage from "./pages/AssignmentPage";
+import CreateAssignmentPage from "./pages/CreateAssignmentPage";
 
 function App() {
   return (
     <>
-      <Routes >
-        <Route path="/login" element={<LoginPage />}/>
-        <Route path="/signup" element={<SignupPage />}/>
-        <Route path="/home" element={ <HomePage /> } />
+      <Routes>
+        <Route path="/login" element={<LoginPage />} />
+        <Route path="/signup" element={<SignupPage />} />
+        <Route path="/home" element={<HomePage />} />
         <Route path="/peoples" element={<PeoplePage />} />
         <Route path="/account" element={<AccountPage />} />
-        <Route path="/edit-account" element={ <EditAccountPage />} />
-        <Route path="/assignment" element={ <AssignmentPage /> } />
+        <Route path="/edit-account" element={<EditAccountPage />} />
+        <Route path="/assignment" element={<AssignmentPage />} />
+        <Route path="/assignment/create" element={<CreateAssignmentPage />} />
         <Route path="*" element={<Navigate to="/login" replace />} />
         <Route path="/people" element={<PeoplePage />} />
       </Routes>
