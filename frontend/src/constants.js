a0965a5173cb674967cdaf62566baba8cb8ffa47
David-Yan-22 <73031516+David-Yan-22@users.noreply.github.com>
2025-10-21 22:07:07 +1100

    Deploy: modify url logic to swith local and deployment url freely

--- frontend/src/constants.js
+++ frontend/src/constants.js
@@ -1,3 +1,6 @@
+
+const DEPLOY_API="https://68d69d40-72ab-4896-bc17-b3c190219391-dev.e1-us-east-azure.choreoapis.dev/assignment-moderation/backend/v1.0"
+
 export function getApiBaseUrl() {
-    return window.configs?.API_BASE_URL || "http://localhost:8000";
+    return import.meta.env.VITE_API_URL ?  import.meta.env.VITE_API_URL : DEPLOY_API;
 }
