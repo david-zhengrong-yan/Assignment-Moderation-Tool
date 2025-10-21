1c00f84e273414d8fb60f78ed7de86f31675bfcf
David-Yan-22 <73031516+David-Yan-22@users.noreply.github.com>
2025-10-21 15:28:02 +1100

    Deployment backend: use pymysql to replace mysqlclient

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -13,11 +13,11 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 from pathlib import Path
 import os
 from corsheaders.defaults import default_headers
-# import pymysql
+import pymysql
 from dotenv import load_dotenv
 
 load_dotenv()
-# pymysql.install_as_MySQLdb()
+pymysql.install_as_MySQLdb()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
