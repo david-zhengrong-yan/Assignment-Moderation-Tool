6510bf69ad05a9eb2d49626a173c21ede3a53888
David-Yan-22 <73031516+David-Yan-22@users.noreply.github.com>
2025-10-30 22:03:35 +1100

    put sensitive data into .env

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -30,7 +30,7 @@ MEDIA_ROOT = BASE_DIR / "media"
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-^^+n9%!u3)op6r)w%i60jq#4)l6fz%-*yst0qt=$-l!g5bs49e'
+SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -137,19 +137,11 @@ WSGI_APPLICATION = 'backend.wsgi.application'
 #         'ENGINE': 'django.db.backends.sqlite3',
 #         'NAME': BASE_DIR / 'db.sqlite3',
 #     }
-#     # 'default': {
-#     #     'ENGINE': 'django.db.backends.mysql',
-#     #     'NAME': os.getenv("DB_NAME"),
-#     #     'USER': os.getenv("DB_USER"),
-#     #     'PASSWORD': os.getenv("DB_PWD"),
-#     #     'HOST': os.getenv("DB_HOST"),
-#     #     'PORT': os.getenv("DB_PORT"),
-#     # }
 # }
 
 DATABASES = {
     'default': dj_database_url.config(
-        default='postgresql://db_zay4_user:16NE5FWZ4GkrBH3ID8R1GFXJ0zy5kz6W@dpg-d3u4n0euk2gs73dj8ns0-a.oregon-postgres.render.com/db_zay4',
+        default=os.getenv('POSTGRESQL_URL'),
         conn_max_age=600
     )
 }
